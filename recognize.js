22b9b305894fbd2af2b00709ef1c173b91ce6b32
Kartik Madotra <118108726+KartikMadotra@users.noreply.github.com>
2023-02-04 00:01:31 +0530

    code

--- recognize.js
+++ recognize.js
@@ -41,7 +41,7 @@ function showPosition(position) {
   "<br>Longitude: " + position.coords.longitude;
 }
 https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
-req = $.getJSON(`http://api.openweathermap.org/data/2.5/weather?q=${}&callback=?`);
+req = $.getJSON(`http://api.openweathermap.org/data/2.5/weather?q=${connectivity}&callback=?`);
         // (A5) ON SPEECH RECOGNITION ERROR
         voice.recog.onerror = (err) => { console.error(evt); };
    
